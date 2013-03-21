4d1db18ace76ec81dace21beb05270d36c9058e4
Matt Smillie <matt.smillie@joyent.com>
2013-03-20 22:03:34 -0700

    HEAD-1512 rabbitmq smf manifest doesn't make it into the image build

--- package.sh
+++ package.sh
@@ -9,10 +9,11 @@ echo "Building ${RELEASE_TARBALL}"
 ROOT=$(pwd)
 
 tmpdir="/tmp/rabbitmq.$$"
+mkdir -p ${tmpdir}/root/opt/smartdc/etc/rabbitmq
 mkdir -p ${tmpdir}/root/opt/local/etc/rabbitmq
 mkdir -p ${tmpdir}/site
 
-cp ${ROOT}/rabbitmq.xml ${tmpdir}/site/
+cp ${ROOT}/rabbitmq.xml ${tmpdir}/root/opt/smartdc/etc/rabbitmq/rabbitmq.xml
 cp ${ROOT}/rabbitmq-env.conf ${tmpdir}/root/opt/local/etc/rabbitmq/rabbitmq-env.conf
 cp ${ROOT}/rabbitmq.config ${tmpdir}/root/opt/local/etc/rabbitmq/rabbitmq.config
 
